b7cd3efd9b019ef72832b9e15a0932851b26a3a9
CoolingJam <CoolingJam19341@gmail.com>
2023-07-11 22:54:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# A simple brainfuck interpeter
+
+This is a very simple brainfuck interpeter written in C. 
+
+Limitations:
+- Can only read from one line in a file
+
+# Usage
+The usage of this program is: <file>
+
+For example, to run a file called hello.br:
+`brainfuck.exe hello.br`
---
+++ main.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Size of the brainfuck array
+#define arraySize 30000
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Not enough arguments. Usage: <file>");
+        return 1;
+    }
+    if (argc > 2) {
+        printf("Too many arguments. Usage: <file>");
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) { 
+        printf("Cannot read file");
+        return 1;
+    }
+
+    char data[arraySize] = { 0 };
+    char instructions[arraySize] = { 0 };
+    int dataPointer = 0;
+    int instructionPointer = 0;
+    int loopDepth = 0;
+
+    // TODO: Make this accept multi line files
+    fgets(instructions, arraySize, file); 
+    fclose(file);   
+
+    while(instructions[instructionPointer]) {
+        switch(instructions[instructionPointer]) {
+            case '>':
+                dataPointer++;
+                break;
+            case '<':
+                dataPointer--;
+                break;
+            case '+':
+                data[dataPointer]++;
+                break;
+            case '-':
+                data[dataPointer]--;
+                break;
+            case '.':
+                printf("%c", data[dataPointer]);
+                break;
+            case ',':
+                scanf("%c", &data[dataPointer]);
+                break;
+            case ';':
+                printf("Debug statement indeed\n");
+                break;
+            case '[':
+                if (!data[dataPointer]) {
+                    instructionPointer++;
+                    while(true) {
+                        char instruction = instructions[instructionPointer];
+                        if (instruction == ']' && loopDepth == 0) break;
+                        if (instruction == ']') {
+                            loopDepth--;
+                        } else if (instruction == '[') {
+                            loopDepth++;
+                        }
+                        instructionPointer++;
+                    }
+                }
+                break;
+            case ']':
+                if (data[dataPointer]) {
+                    while(true) {
+                        char instruction = instructions[instructionPointer];
+                        if (instruction == '[') {
+                            loopDepth--;
+                        } else if (instruction == ']') {
+                            loopDepth++;
+                        }
+                        if (instruction == '[' && loopDepth == 0) {
+                            break;
+                        }
+                        instructionPointer--;
+                    }
+                }
+                break;
+        }
+        instructionPointer++;
+    }
+
+}
